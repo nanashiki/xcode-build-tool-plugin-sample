beaa35b061607d4d6483161408348db86585a7b3
nanashiki <nanashiki.app@gmail.com>
2023-05-31 01:25:10 +0900

    Initial Commit

---
+++ XcodeBuildToolPluginSample/XcodeBuildToolPluginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeBuildToolPluginSampleApp.swift
+//  XcodeBuildToolPluginSample
+//
+//  Created by nanashiki on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct XcodeBuildToolPluginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
